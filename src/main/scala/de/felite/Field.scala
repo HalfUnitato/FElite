65314e5f79cbc26e9111ad52de062ccc5091eddd
Lukas Brandt <lukas.brandt@htwg-konstanz.de>
2019-10-25 17:13:05 +0200

    Add: field size set on 20*20

--- src/main/scala/de/felite/Field.scala
+++ src/main/scala/de/felite/Field.scala
@@ -1,7 +1,23 @@
 package de.felite
 
 case class Field(initFile: Array[Array[Char]]) { //: Array.ofDim[char]()){
-  private val field = initFile
+  private val matrix = Array.ofDim[Char](20,20)
+
+  // All Grass
+  for (i <- 0 until 20) {
+    for (j <- 0 until 20) {
+      matrix(i)(j) = Grass.sign
+    }
+  }
+
+  for (i <- 0 until 20) {
+    for (j <- 0 until 20) {
+      print(matrix(i)(j))
+    }
+    println()
+  }
+
+  /*private val field = initFile
   for (x <- initFile.indices)
     for (y <- x.)
       field(y)(x) = initFile(y)(x) match {
@@ -9,5 +25,5 @@ case class Field(initFile: Array[Array[Char]]) { //: Array.ofDim[char]()){
         case Grass.sign => Cell(Grass)
         case Rock.sign => Cell(Rock)
         case _ => Cell(Grass)
-      }
+      }*/
 }
--- src/test/scala/de/felite/CellTesting.sc
+++ src/test/scala/de/felite/CellTesting.sc
@@ -1,10 +1,40 @@
-import de.felite.Cell
-
+import de.felite._
 val i = Array(1,2,3)
-val field = Array.ofDim[Cell](3,3)
+//val field = Array.ofDim[Cell](3,3)
+val arr = Array(Char,Char)
+val fullArr = Array.ofDim[Char](20, 20)
+for (i <- 0 until 20) {
+ for (j <- 0 until 20) {
+  fullArr(i)(j) = '*'
+ }
+}
+
+var k = 5
+for (i <- 0 until 7) {
+ for (j <- 0 until k) {
+  fullArr(i)(j) = 't'
+ }
+ k -= 1
+}
+
 
  val field = Array.ofDim[Char](2,2)
-   {{'a','b'}
+for (i <- 0 to 1) {
+ for (j <- 0 to 1) {
+  //  println(i,j)
+  field(i)(j) = 20
+ }
+}
+
+
+for (i <- 0 until 20) {
+ for (j <- 0 until 20) {
+  print(fullArr(i)(j) + " ")
+ }
+ println()
+}
+
+/* {{'a','b'}
   for(x <-  field.){
 
-  }
+  }*/
