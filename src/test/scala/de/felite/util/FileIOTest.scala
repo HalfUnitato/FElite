8bfb9a3a01b157af336f1daa2b3c0cb8ad5d091d
Lukas Brandt <lukas.brandt@htwg-konstanz.de>
2019-11-21 13:42:27 +0100

    travis tests 0

--- src/test/scala/de/felite/util/FileIOTest.scala
+++ src/test/scala/de/felite/util/FileIOTest.scala
@@ -6,7 +6,7 @@ class FileIOTest extends TestBaseClass {
   "A FileIO" when {
     "readFromFIle" should {
       "not throw an Exception " in {
-        noException shouldBe thrownBy(FileIO.readFromFile("src\\fieldTest.txt"))
+        noException shouldBe thrownBy(FileIO.readFromFile("FElite\\src\\fieldTest.txt"))
       }
     }
   }
